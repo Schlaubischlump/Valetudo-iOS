1040c76a3b9cca4e991341e14a4c8b5bc461af6f
SchlaubiSchlump <->
2026-04-28 22:03:12 +0200

    Fix updateBorder function

--- Valetudo/Source/Extension/UIView + Extension.swift
+++ Valetudo/Source/Extension/UIView + Extension.swift
@@ -8,34 +8,36 @@ import UIKit
 
 extension UIView {
     func updateBorder(edge: UIRectEdge, color: UIColor, thickness: CGFloat) {
-        let borderName: String
-        switch edge {
-        case .top: borderName = "topBorder"
-        case .bottom: borderName = "bottomBorder"
-        case .left: borderName = "leftBorder"
-        case .right: borderName = "rightBorder"
-        default: return
-        }
+        let supportedEdges: [(UIRectEdge, String)] = [
+            (.top, "topBorder"),
+            (.bottom, "bottomBorder"),
+            (.left, "leftBorder"),
+            (.right, "rightBorder"),
+        ]
 
-        let existingBorder = layer.sublayers?.first(where: { $0.name == borderName })
+        for (supportedEdge, borderName) in supportedEdges {
+            guard edge.contains(supportedEdge) else { continue }
 
-        let frame: CGRect = switch edge {
-        case .top: CGRect(x: 0, y: 0, width: bounds.width, height: thickness)
-        case .bottom: CGRect(x: 0, y: bounds.height - thickness, width: bounds.width, height: thickness)
-        case .left: CGRect(x: 0, y: 0, width: thickness, height: bounds.height)
-        case .right: CGRect(x: bounds.width - thickness, y: 0, width: thickness, height: bounds.height)
-        default: .zero
-        }
+            let existingBorder = layer.sublayers?.first(where: { $0.name == borderName })
 
-        if let border = existingBorder {
-            border.frame = frame
-            border.backgroundColor = color.cgColor
-        } else {
-            let border = CALayer()
-            border.name = borderName
-            border.backgroundColor = color.cgColor
-            border.frame = frame
-            layer.addSublayer(border)
+            let frame: CGRect = switch supportedEdge {
+            case .top: CGRect(x: 0, y: 0, width: bounds.width, height: thickness)
+            case .bottom: CGRect(x: 0, y: bounds.height - thickness, width: bounds.width, height: thickness)
+            case .left: CGRect(x: 0, y: 0, width: thickness, height: bounds.height)
+            case .right: CGRect(x: bounds.width - thickness, y: 0, width: thickness, height: bounds.height)
+            default: .zero
+            }
+
+            if let border = existingBorder {
+                border.frame = frame
+                border.backgroundColor = color.cgColor
+            } else {
+                let border = CALayer()
+                border.name = borderName
+                border.backgroundColor = color.cgColor
+                border.frame = frame
+                layer.addSublayer(border)
+            }
         }
     }
 
